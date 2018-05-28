af65f1e5dd6d8d1a71e1827320b2f764670463ac
Julio Perez <juliolpj@hotmail.com>
2018-05-28 14:49:10 -0400

    Instalar dependencias

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyDjUTlxnxx8spYLmds-DdzhfdJKSTKYhnc',
+    authDomain: 'fb02-840d8.firebaseapp.com',
+    databaseURL: 'https://fb02-840d8.firebaseio.com',
+    projectId: 'fb02-840d8',
+    storageBucket: 'fb02-840d8.appspot.com',
+    messagingSenderId: '901688334598'
+  }
+};
