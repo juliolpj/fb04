d121d4da4610ad913a83d28bc3e0237f4a6bc16d
Julio Perez <juliolpj@hotmail.com>
2018-05-28 16:19:08 -0400

    navbar y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 import { PrivatePageComponent } from './components/private-page/private-page.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
@@ -24,6 +25,7 @@ import { RegisterPageComponent } from './components/register-page/register-page.
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule
   ],
   providers: [
