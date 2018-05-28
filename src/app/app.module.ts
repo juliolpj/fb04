5c0bdd5487f09b4414d95161c37d45a232bcf596
Julio Perez <juliolpj@hotmail.com>
2018-05-28 15:16:35 -0400

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { PrivatePageComponent } from './components/private-page/private-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    NavbarComponent,
+    NotFoundPageComponent,
+    PrivatePageComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
